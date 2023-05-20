23df90e0bc4efe7f14463165e8ec7253a6809ee4
Ivan Zimmerman <ivan.g.zimmerman@gmail.com>
2023-05-19 21:28:10 -0700

    args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    apcsp::run("./csp/simple.csp");
+    let path = match std::env::args().nth(1) {
+        Some(path) => path,
+        None => {
+            eprintln!("\nError: could not find path\n\nUsage: `cargo run -- <path>.csp`\ne.g. `cargo run -- ./csp/simple.csp`");
+            std::process::exit(1);
+        }
+    };
+
+    apcsp::run(&path);
 }
